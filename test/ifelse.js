2996b2fb27a6b45a2ec9d422619cd5b5f36ed731
Agata Bobrowska <agata.m.bobrowska@gmail.com>
2025-04-22 15:22:20 +0200

    if else

---
+++ test/ifelse.js
@@ -0,0 +1,17 @@
+const grade = 45;
+
+if(grade>= 90) {
+    console.log("Exellent");
+}
+else if(grade >= 80) {
+    console.log("Good");
+}
+else if(grade >= 70) {
+    console.log("Average");
+}
+else if(grade >= 60) {
+    console.log("Pass");
+}
+else {
+    console.log("Fail");
+} // Exellent
